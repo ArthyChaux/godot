6b1077a624d2b16b521838b354945e75de1ea391
Phoenixcresset <jeanbaptiste.lambertin@gmail.com>
2023-11-15 12:14:55 +0100

    Premier test qui marche

--- tests/scene/test_camera_2d.h
+++ tests/scene/test_camera_2d.h
@@ -32,11 +32,27 @@
 #define TEST_CAMERA_2D_H
 
 #include "scene/2d/camera_2d.h"
-
+#include "scene/main/viewport.h"
 #include "scene/main/window.h"
 #include "tests/test_macros.h"
 
 namespace TestCamera2D {
+
+TEST_CASE("[SceneTree][Camera2D] Getters and setters") {
+	Camera2D *test_camera = memnew(Camera2D);
+
+	SUBCASE("AnchroMode") {
+		test_camera->set_anchor_mode(Camera2D::AnchorMode::ANCHOR_MODE_FIXED_TOP_LEFT);
+		CHECK(test_camera->get_anchor_mode() == Camera2D::AnchorMode::ANCHOR_MODE_FIXED_TOP_LEFT);
+
+
+
+	}
+
+
+
+}
+
 
 } // namespace TestCamera2D
 
